dd4fd3ec7f34a4ccf3cdac6110b990651c9afc05
Daniel Veyde <daniel.v11@gso.schule.koeln>
2022-12-09 09:14:34 +0100

    zurBesprechung

---
+++ Fuhrparkverwaltung/Auto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using 
+
+namespace Fuhrparkverwaltung
+{
+    public class Auto
+    {
+
+        private int kilometerstand;
+        public Auto(int kilometerstand)
+        {
+            kilometerstand = this.kilometerstand;
+        }
+
+        public void Fahren(int streckeInKilometern)
+        {
+            int gefahreneStrecke += streckeInKilometern; 
+
+            return gefahreneStrecke;
+        }
+
+        public int Kilometerstand
+        {
+           get { return this.kilometerstand; }
+           set { kilometerstand = value; }
+        }
+
+    }
+}
